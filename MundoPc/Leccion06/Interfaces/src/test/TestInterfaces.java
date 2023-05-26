1a4413234e310469481fb442118287573004a1f2
Vera2376 <103527322+Vera2376@users.noreply.github.com>
2023-05-25 22:04:25 -0300

    Comiteamos los archivos de Java del terer semestre

---
+++ MundoPc/Leccion06/Interfaces/src/test/TestInterfaces.java
@@ -0,0 +1,21 @@
+
+package test;
+
+import accesodatos.*;
+
+
+
+public class TestInterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        
+        datos = new ImplementacionOracle();
+        //datos.listar();
+    }
+    
+    public static void imprimir(IAccesoDatos datos){
+        datos.listar();
+    }
+    
+}
